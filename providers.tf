3f0e3601d5f6068b005b35935c97206a10bc4bc7
Gideon Wulfsohn <gideon.wulfsohn@gmail.com>
2022-10-07 11:27:06 -0400

    update variable mismatch in main.tf

--- providers.tf
+++ providers.tf
@@ -17,6 +17,6 @@ provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  private_key_path = var.private_key
   region           = var.region
 }
